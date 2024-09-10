3b181033062c27129aeb500f24a84393db5c738a
George Croucher <gpcroucher@gmail.com>
2024-09-10 13:42:04 +0100

    added /list POST endpoint to add a film to a user's list

--- server/server.js
+++ server/server.js
@@ -17,6 +17,32 @@ app.get("/users", async function (request, response) {
   response.json(users.rows);
 });
 
+app.post("/list", async (request, response) => {
+  console.log("request.body", request.body);
+
+  if (request.body.list == "seenlist") {
+    const post = await db.query(
+      `
+      UPDATE week05projectusers
+      SET seenlist = ARRAY_APPEND(seenlist, $1) 
+      WHERE username = $2`,
+      [request.body.filmID, request.body.username]
+    );
+    response.json(post);
+  } else if (request.body.list == "watchlist") {
+    const post = await db.query(
+      `
+      UPDATE week05projectusers
+      SET watchlist = ARRAY_APPEND(watchlist, $1) 
+      WHERE username = $2`,
+      [request.body.filmID, request.body.username]
+    );
+    response.json(post);
+  } else {
+    response.status(400).json({ error: "invalid list type" });
+  }
+});
+
 app.listen(8080, function () {
   console.log("App is running on port 8080");
 });
