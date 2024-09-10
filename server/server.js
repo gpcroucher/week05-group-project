a3023a32cc0262dabd86b45fc902e6d47952dc5d
Jonathan Lee <kutsu89@gmail.com>
2024-09-10 13:38:40 +0100

    created /list endpoint for watch and seen lists

--- server/server.js
+++ server/server.js
@@ -17,6 +17,27 @@ app.get("/users", async function (request, response) {
   response.json(users.rows);
 });
 
+app.get("/list", async function (request, response) {
+  const query = request.query;
+  console.log(query);
+
+  if (request.query.list === "seen") {
+    const seenlist = await db.query(
+      `SELECT seenlist FROM week05projectusers WHERE username = $1`,
+      [query.user]
+    );
+    response.json(seenlist.rows);
+  } else if (query.list === "watch") {
+    const watchlist = await db.query(
+      `SELECT watchlist FROM week05projectusers WHERE username = $1`,
+      [query.user]
+    );
+    response.json(watchlist.rows);
+  } else {
+    response.status(400).json({ error: "invalid list type" });
+  }
+});
+
 app.listen(8080, function () {
   console.log("App is running on port 8080");
 });
