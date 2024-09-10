f616bdcffc90d1d08daaff5e8f4d6b59c372c968
Jonathan Lee <kutsu89@gmail.com>
2024-09-10 11:25:22 +0100

    made users endpoint that returns all users

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/users", async function (request, response) {
+  const users = await db.query("SELECT * FROM week05projectusers");
+  response.json(users.rows);
+});
+
+app.listen(8080, function () {
+  console.log("App is running on port 8080");
+});
